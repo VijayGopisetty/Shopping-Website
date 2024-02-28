dfe368d7040ad0411b678a73618c23bfca2a5375
Vijay <dhruvakumar.gopisetty@gmail.com>
2024-02-28 13:25:09 +0530

    User-API created

---
+++ src/main/java/com/shopping/backend/user/User/API/controller/SellerController.java
@@ -0,0 +1,12 @@
+package com.shopping.backend.user.User.API.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/seller")
+public class SellerController {
+    public void registerProduct(){
+
+    }
+}
